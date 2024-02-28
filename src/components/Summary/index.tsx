aa027abaff6952fddec3da397ddea34f3ea4593d
Kaique <kaiquedecampos2@gmail.com>
2024-02-28 17:43:29 -0300

    refactor: refactor Summary component to use custom hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,34 +1,14 @@
-import { TransactionsContext } from '@/contexts/TransactionsContext'
+import { useSummary } from '@/hooks/useSummary'
 import { formatCurrency } from '@/utils/format_currency'
 import {
   ArrowCircleDown,
   ArrowCircleUp,
   CurrencyDollar,
 } from '@phosphor-icons/react'
-import { useContext } from 'react'
 import { Card, CardHeader, SummaryContainer, SummaryContent } from './styles'
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext)
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+  const summary = useSummary()
 
   return (
     <SummaryContainer>
